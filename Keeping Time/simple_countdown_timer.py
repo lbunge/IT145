7d17cb14c87eaf50d38ff42cb09c149266ff644d
luke <lukebunge@live.com>
2019-12-02 18:12:12 -0500

    created simple_countdown_timer.py

---
+++ Keeping Time/simple_countdown_timer.py
@@ -0,0 +1,22 @@
+"""
+Simple Countdown Program:
+
+Write a countdown program that measures how long this piece of code takes to run.
+Count down from 60.
+Pause for one second in between displaying each number in the countdown by calling time.sleep().
+Print the time your code takes to run at the end of the countdown.
+"""
+import time
+
+
+def countdown_timer(value):
+    print("Starting now:")
+    for i in range(value, 0, -1):
+        print(i)
+        time.sleep(1)
+    print("Times up!")
+
+
+print("Lets start a timer!")
+value = input("Please enter time in seconds: ")
+countdown_timer(int(value))
